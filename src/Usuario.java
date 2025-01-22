a812e9d4c9a403f26f9fa530664101b68a0f9e3f
AlbertoKimi <breakingdawn1988@gmail.com>
2025-01-22 17:08:42 +0100

    mod usuario

--- src/Usuario.java
+++ src/Usuario.java
@@ -32,5 +32,53 @@ public class Usuario {
         this.tipo=comprobarTipo();
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public char getTipo() {
+        return sexo;
+    }
+
+    public void setTipo(char tipo) {
+        this.tipo = tipo;
+    }
+
 }
 
